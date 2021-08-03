73d1c7cb077b55c7f7b9e2b7f39910f35e630f96
“Wei <43235151+suenxw@users.noreply.github.com>
2021-08-03 21:21:32 +0100

    Write models.py related code

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class AnimalCategory(models.Model):
+    NAME_MAX_LENGTH = 128
+
+    category_name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
+    views = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.category_name)
+        super(AnimalCategory, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Animal Categories'
+
+    def __str__(self):
+        return self.category_name
+
+
+class Animal(models.Model):
+    NAME_MAX_LENGTH = 128
+
+    animal_name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
+    category = models.ForeignKey(AnimalCategory, on_delete=models.CASCADE)
+
+    likes = models.IntegerField(default=0)
+    brief = models.CharField(max_length=NAME_MAX_LENGTH)
+    picture = models.ImageField(upload_to='animals_images', blank=True)
+
+    def __str__(self):
+        return self.animal_name
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # website = models.URLField(blank=True)
+    phone = models.CharField(max_length=15)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Comment(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(max_length=255)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content[:20]
